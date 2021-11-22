df75b2abc63af817a6458d65a0828084bea31dc0
Grant Rinehimer <gwr47@cornell.edu>
2021-11-21 19:10:50 -0500

    Finish backend midpoint

--- backend/src/app.py
+++ backend/src/app.py
@@ -4,13 +4,11 @@ from flask import Flask
 from flask import request
 import json
 
-from db import Course
-from db import db
 from db import User
-from db import Assignment
+from db import db
 
 app = Flask(__name__)
-db_filename = "cms.db"
+db_filename = "tennisTrainer.db"
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///%s" % db_filename
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
@@ -30,112 +28,40 @@ def failure_response(message, code=404):
     return json.dumps({"error": message}), code
 
 
-@app.route("/api/courses/")
-def get_courses():
+@app.route("/api/user/")
+def get_players():
     return success_response(
-        {"courses": [c.serialize() for c in Course.query.all()]}
+        {"users": [u.serialize() for u in User.query.all()]}
     )
 
 
-@app.route("/api/courses/", methods=["POST"])
-def create_course():
-    body = json.loads(request.data)
-    code = body.get("code")
-    name = body.get("name")
-    if name is None or code is None:
-        return failure_response("Did not provide required data.", 400)
-
-    new_course = Course(code=code, name=name)
-
-    db.session.add(new_course)
-    db.session.commit()
-    return success_response(new_course.serialize(), 201)
-
-
-@app.route("/api/courses/<int:course_id>/")
-def get_course(course_id):
-    course = Course.query.filter_by(id=course_id).first()
-    if course is None:
-        return failure_response("Course not found.")
-    return success_response(course.serialize())
-
-
-@app.route("/api/courses/<int:course_id>/", methods=['DELETE'])
-def delete_course(course_id):
-    course = Course.query.filter_by(id=course_id).first()
-    if course is None:
-        return failure_response("Course not found.")
-    db.session.delete(course)
-    db.session.commit()
-    return success_response(course.serialize())
-
-
-@app.route("/api/courses/<int:course_id>/assignment/", methods=["POST"])
-def create_assignment(course_id):
-    course = Course.query.filter_by(id=course_id).first()
-    if course is None:
-        return failure_response("Course not found.")
-
-    body = json.loads(request.data)
-    title = body.get("title")
-    due_date = body.get("due_date")
-    if title is None or due_date is None:
-        return failure_response("Did not provide required data.", 400)
-
-    new_assignment = Assignment(title=title, due_date=due_date, course_id=course_id)
-
-    db.session.add(new_assignment)
-    db.session.commit()
-    return success_response(new_assignment.serialize(), 201)
-
-
-@app.route("/api/users/", methods=['POST'])
+@app.route("/api/user/", methods=["POST"])
 def create_user():
     body = json.loads(request.data)
     name = body.get("name")
-    netid = body.get("netid")
-    if name is None or netid is None:
+    email = body.get("email")
+    username = body.get("username")
+    uType = body.get("uType")
+
+    if name is None or email is None or username is None or uType is None:
         return failure_response("Did not provide required data.", 400)
 
-    new_user = User(name=name, netid=netid)
+    if uType != 0 and uType != 1:
+        return failure_response("uType must be 0 (player) or 1 (coach)", 400)
+
+    new_user = User(name=name, email=email, username=username, uType=uType)
 
     db.session.add(new_user)
     db.session.commit()
     return success_response(new_user.serialize(), 201)
 
 
-@app.route("/api/users/<int:user_id>/")
-def get_user(user_id):
-    user = User.query.filter_by(id=user_id).first()
+@app.route("/api/user/<int:uid>/")
+def get_user(uid):
+    user = User.query.filter_by(uid=uid).first()
     if user is None:
         return failure_response("User not found.")
     return success_response(user.serialize())
-
-
-@app.route("/api/courses/<int:course_id>/add/", methods=['POST'])
-def add_user_to_course(course_id):
-    course = Course.query.filter_by(id=course_id).first()
-    if course is None:
-        return failure_response("Course not found.")
-
-    body = json.loads(request.data)
-    user_id = body.get("user_id")
-    type = body.get("type")
-    if user_id is None or not (type == "student" or type == "instructor"):
-        return failure_response("Incorrect data provided.", 400)
-
-    user = User.query.filter_by(id=user_id).first()
-    if user is None:
-        return failure_response("User not found.")
-
-    if type == "student":
-        course.students.append(user)
-    else:
-        course.teachers.append(user)
-
-    db.session.commit()
-
-    return success_response(course.serialize())
 
 
 if __name__ == "__main__":
